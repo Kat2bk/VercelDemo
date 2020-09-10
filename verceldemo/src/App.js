45dd967b7a96d68c285783933c20d5618d31af3d
unknown <Katherine2bk@gmail.com>
2020-09-09 18:10:46 -0700

    pushing changes

--- verceldemo/src/App.js
+++ verceldemo/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const [background, setBackground] = useState()
+
   return (
     <div className="App">
       <header className="App-header">
        <h1>Wasssup!</h1>
+       
       </header>
     </div>
   );
